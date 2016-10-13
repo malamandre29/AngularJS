e5fe96675f52c80e3be6c79583a1f101ca884b8d
malamandre29 <nasioulas.v@gmail.com>
2016-10-13 15:47:13 +0300

    Module 2 final solution

---
+++ module2-solution/app.js
@@ -0,0 +1,73 @@
+(function () {
+	'use strict';
+	angular.module('ShoppingListCheckOff', [])
+	.controller('ToBuyController', ToBuyController)
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+	ToBuyController.$inject = ['ShoppingListCheckOffService'];
+	function ToBuyController(ShoppingListCheckOffService) {
+		this.list = ShoppingListCheckOffService.getToBuyItems() ;
+		this.switchItem = function(itemIndex){
+			ShoppingListCheckOffService.switchService(itemIndex);
+		};
+
+		this.errorMessage = function () {
+			if (this.list.length === 0) {
+				return true;
+			}
+			else return false;
+		};
+	}
+
+	AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+	function AlreadyBoughtController(ShoppingListCheckOffService) {
+		this.list = ShoppingListCheckOffService.getBoughtItems();
+
+		this.errorMessage = function () {
+			if (this.list.length === 0) {
+				return true;
+			}
+			else return false;
+		};
+		
+	}
+
+	function ShoppingListCheckOffService() {
+		var bought = [];
+
+		var to_buy = [
+			{
+				name: "Milk",
+				quantity: "7"
+			} ,
+			{
+				name: "Donuts",
+				quantity: "10"
+			} ,
+			{
+				name: "Souvlaki",
+				quantity: "15"
+			} ,
+			{
+				name: "Donuts",
+				quantity: "10"
+			}
+		];
+		this.getToBuyItems = function() {
+			return to_buy;
+		};
+
+		this.getBoughtItems = function() {
+			return bought;
+		};
+		this.switchService = function(itemIndex) {
+			bought.push(to_buy[itemIndex]);
+			to_buy.splice(itemIndex,1);
+		}
+		
+	}
+
+
+
+})();
